a665bdca2e4dd3b732e89ffdb39bdd138173a49a
LuminiteTime <trifonov2812@gmail.com>
2025-02-15 23:09:23 +0300

    FEATURE: Add code style

--- popup.js
+++ popup.js
@@ -38,11 +38,28 @@ function updateTheme(isDark) {
     for (let pre of preTags) {
       pre.style.backgroundColor = '#1E1E1E';
       pre.style.color = '#E1E1E1';
+      
+      const code = pre.querySelector('code');
+      if (code) {
+        code.style.color = '#50FA7B';
+      }
     }
+
+    const codeWrappers = document.getElementsByClassName('code-block-wrapper');
+    for (let wrapper of codeWrappers) {
+      wrapper.style.outline = 'none';
+      wrapper.addEventListener('mouseenter', () => {
+        wrapper.style.outline = `2px solid #50FA7B`;
+      });
+      wrapper.addEventListener('mouseleave', () => {
+        wrapper.style.outline = 'none';
+      });
+    }
+    
     const blockquotes = document.getElementsByTagName('blockquote');
     for (let blockquote of blockquotes) {
       blockquote.style.color = '#E1E1E1';
-      blockquote.style.borderLeft = '.25em solid #6B4BFF';
+      blockquote.style.borderLeft = '.25em solid #A5EC60';
     }
   } else {
     document.body.style.color = '#333';
@@ -51,7 +68,24 @@ function updateTheme(isDark) {
     for (let pre of preTags) {
       pre.style.backgroundColor = '#f5f5f5';
       pre.style.color = '#333';
+
+      const code = pre.querySelector('code');
+      if (code) {
+        code.style.color = '#0033CC';
+      }
     }
+
+    const codeWrappers = document.getElementsByClassName('code-block-wrapper');
+    for (let wrapper of codeWrappers) {
+      wrapper.style.outline = 'none';
+      wrapper.addEventListener('mouseenter', () => {
+        wrapper.style.outline = `2px solid #0033CC`;
+      });
+      wrapper.addEventListener('mouseleave', () => {
+        wrapper.style.outline = 'none';
+      });
+    }
+    
     const blockquotes = document.getElementsByTagName('blockquote');
     for (let blockquote of blockquotes) {
       blockquote.style.color = '#777';
